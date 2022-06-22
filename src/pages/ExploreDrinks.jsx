70eddc53a6d81ca014595f65aff17df85884831e
Andre Rousselet <andre098@hotmail.com>
2022-06-22 16:47:35 -0300

    Add buttons inside Link, and functions to generate path with random recipe

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import RecipesContext from '../context/RecipesContext';
 
 function ExploreDrinks() {
+  const { data } = useContext(RecipesContext);
+
+  const randomRecipePath = () => {
+    if (data.length !== 0) {
+      const randomIndex = Math.floor(Math.random() * data.length);
+      return `/drinks/${data[randomIndex].idDrink}`;
+    }
+  };
+
   return (
-    <>
-      <div>
-        <Header pageTitle="Explore Drinks" />
-        dsda
-      </div>
-      <footer>
-        <Footer />
-      </footer>
-
-    </>
-
+    <div>
+      <Header pageTitle="Explore Drinks" />
+      <Link to="/explore/drinks/ingredients">
+        <button type="button" data-testid="explore-by-ingredient">
+          By Ingredient
+        </button>
+      </Link>
+      <Link to={ randomRecipePath() }>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Surprise me!
+        </button>
+      </Link>
+      <Footer />
+    </div>
   );
 }
 
--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import RecipesContext from '../context/RecipesContext';
 
 function ExploreFoods() {
+  const { data } = useContext(RecipesContext);
+
+  const randomRecipePath = () => {
+    if (data.length !== 0) {
+      const randomIndex = Math.floor(Math.random() * data.length);
+      return `/foods/${data[randomIndex].idMeal}`;
+    }
+  };
+
   return (
-    <>
-      <div>
-        <Header pageTitle="Explore Foods" />
-        dsda
-      </div>
-      <footer>
-        <Footer />
-      </footer>
-
-    </>
-
+    <div>
+      <Header pageTitle="Explore Foods" />
+      <Link to="/explore/foods/ingredients">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          By Ingredient
+        </button>
+      </Link>
+      <Link to="/explore/foods/nationalities">
+        <button
+          type="button"
+          data-testid="explore-by-nationality"
+        >
+          By Nationality
+        </button>
+      </Link>
+      <Link to={ randomRecipePath() }>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Surprise me!
+        </button>
+      </Link>
+      <Footer />
+    </div>
   );
 }
 

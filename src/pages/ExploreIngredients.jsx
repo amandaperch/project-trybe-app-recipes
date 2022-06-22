0a5c08336d27a0f9599579de527ea220ba900f5a
Andre Rousselet <andre098@hotmail.com>
2022-06-22 19:32:05 -0300

    Fetch diferent data from API depending on pathname

--- src/pages/ExploreIngredients.jsx
+++ src/pages/ExploreIngredients.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-function ExploreIngredients() {
-  return (
-    <>
-      <div>
-        <Header pageTitle="Explore Ingredients" />
-        dsda
-      </div>
-      <footer>
-        <Footer />
-      </footer>
+const MEALS_INGREDIENTS_URL = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+const DRINKS_INGREDIENTS_URL = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list';
 
-    </>
+function ExploreIngredients() {
+  const [ingredients, setIngredients] = useState();
+  const history = useHistory();
+
+  const getMealIngredients = async () => {
+    const response = await fetch(MEALS_INGREDIENTS_URL);
+    const { meals } = await response.json();
+    setIngredients(meals);
+  };
+
+  const getDrinksIngredients = async () => {
+    const response = await fetch(DRINKS_INGREDIENTS_URL);
+    const { drinks } = await response.json();
+    setIngredients(drinks);
+  };
+
+  useEffect(() => {
+    if (history.location.pathname.includes('foods')) {
+      getMealIngredients();
+    }
+    getDrinksIngredients();
+  }, [history]);
+
+  return (
+    <div>
+      <Header pageTitle="Explore Ingredients" />
+      blabla
+      <Footer />
+    </div>
   );
 }
 

dda0c513342a22330040d6d695020bd07e0a5648
Andre Rousselet <andre098@hotmail.com>
2022-06-22 21:12:58 -0300

    Renders list of ingredients using IngredientCard

--- src/pages/ExploreIngredients.jsx
+++ src/pages/ExploreIngredients.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import IngredientCard from '../components/IngredientCard';
 
 const MEALS_INGREDIENTS_URL = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
 const DRINKS_INGREDIENTS_URL = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list';
+const TWELVE = 12;
 
 function ExploreIngredients() {
   const [ingredients, setIngredients] = useState();
@@ -13,26 +15,40 @@ function ExploreIngredients() {
   const getMealIngredients = async () => {
     const response = await fetch(MEALS_INGREDIENTS_URL);
     const { meals } = await response.json();
-    setIngredients(meals);
+    setIngredients(meals.filter((_, index) => index < TWELVE));
   };
 
   const getDrinksIngredients = async () => {
     const response = await fetch(DRINKS_INGREDIENTS_URL);
     const { drinks } = await response.json();
-    setIngredients(drinks);
+    setIngredients(drinks.filter((_, index) => index < TWELVE));
   };
 
   useEffect(() => {
     if (history.location.pathname.includes('foods')) {
       getMealIngredients();
     }
-    getDrinksIngredients();
+    if (history.location.pathname.includes('drinks')) {
+      getDrinksIngredients();
+    }
   }, [history]);
 
   return (
     <div>
       <Header pageTitle="Explore Ingredients" />
-      blabla
+      {!ingredients ? <p>loading</p> : (
+        ingredients.map((ingredient, index) => (
+          <Link
+            to="/foods"
+            key={ index }
+          >
+            <IngredientCard
+              infoIngredient={ ingredient }
+              index={ index }
+            />
+          </Link>
+        ))
+      )}
       <Footer />
     </div>
   );

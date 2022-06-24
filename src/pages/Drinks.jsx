9ff19500d0bf310c6e89b6b38e5d2c779aebf38b
Heron Baruch <33159457+heronbaruch@users.noreply.github.com>
2022-06-24 18:09:14 -0300

    filtro atravez das categorias de comidas ou bebidas, de acordo com a pagina

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,18 +1,18 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useCallback } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import RecipesContext from '../context/RecipesContext';
 import Card from '../components/Card';
 import Footer from '../components/Footer';
 
+const maxRecipes = 12;
 function Drinks() {
   const pageTitle = 'Drinks';
-  const { data, setApiFilter, category } = useContext(RecipesContext);
+  const { data, setApiFilter, category, setData } = useContext(RecipesContext);
   const history = useHistory();
 
   useEffect(() => {
     if (data.length === 1) {
-      console.log(data[0]);
       return history.push(`/drinks/${data[0].idDrink}`);
     }
   }, [data, history]);
@@ -25,8 +25,16 @@ function Drinks() {
     }
   }, [data, setApiFilter]);
 
+  const filterCategory = useCallback(async (categoryName) => {
+    const url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=';
+    const { drinks } = await fetch(`${url}${categoryName}`)
+      .then((response) => response.json());
+    setData(drinks.slice(0, maxRecipes));
+  }, [setData]);
+
   return (
     <div>
+      <Header pageTitle={ pageTitle } btnSearch />
       {!category ? undefined : (
         category.map((categoryName, index) => (
           <button
@@ -34,13 +42,12 @@ function Drinks() {
             key={ index }
             type="button"
             data-testid={ `${categoryName.strCategory}-category-filter` }
-            onClick={ (e) => console.log(e.target.value) }
+            onClick={ async (event) => filterCategory(event.target.value) }
           >
             { categoryName.strCategory }
           </button>
         ))
       )}
-      <Header pageTitle={ pageTitle } btnSearch />
       {!data ? <p>loading</p> : (
         data.map((recipe, index) => (
           <Link

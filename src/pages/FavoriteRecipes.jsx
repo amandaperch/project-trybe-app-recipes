ef33eb8ea99d567a6c57b21fa23afc9a6cc134cb
Andre Rousselet <andre098@hotmail.com>
2022-06-29 20:05:03 -0300

    Filters favoriteRecipes using filter state comparisons

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,33 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FavoriteCard from '../components/FavoriteCard';
 import Header from '../components/Header';
 
 function FavoriteRecipes() {
+  const [filter, setFilter] = useState('All');
   const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+
   return (
     <div>
       <Header pageTitle="Favorite Recipes" />
       <button
         type="button"
         data-testid="filter-by-all-btn"
+        onClick={ () => setFilter('All') }
       >
         All
       </button>
       <button
         type="button"
         data-testid="filter-by-food-btn"
+        onClick={ () => setFilter('Food') }
       >
         Food
       </button>
       <button
         type="button"
         data-testid="filter-by-drink-btn"
+        onClick={ () => setFilter('Drinks') }
       >
         Drinks
       </button>
-      {favoriteRecipes && favoriteRecipes.map((favRecipe, index) => (
-        <FavoriteCard key={ favRecipe.id } infoRecipe={ favRecipe } index={ index } />
-      ))}
+      {favoriteRecipes
+        && favoriteRecipes
+          .filter((favRecipe) => {
+            if (filter === 'Food') {
+              return favRecipe.type === 'food';
+            }
+            if (filter === 'Drinks') {
+              return favRecipe.type === 'drink';
+            }
+            return favRecipe;
+          })
+          .map((favRecipe, index) => (
+            <FavoriteCard key={ favRecipe.id } infoRecipe={ favRecipe } index={ index } />
+          ))}
     </div>
   );
 }

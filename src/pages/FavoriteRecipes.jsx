90e99560da95dbb8760d23e03bc4caab26762a1b
Andre Rousselet <andre098@hotmail.com>
2022-06-29 19:27:28 -0300

    Get favorite recipes from localStorage, adds filter buttons(no functions yet), renders FavoriteCard passing props

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import FavoriteCard from '../components/FavoriteCard';
 import Header from '../components/Header';
 
 function FavoriteRecipes() {
+  const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
   return (
     <div>
       <Header pageTitle="Favorite Recipes" />
-      dsda
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+      >
+        Food
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+      >
+        Drinks
+      </button>
+      {favoriteRecipes && favoriteRecipes.map((favRecipe) => (
+        <FavoriteCard key={ favRecipe.id } infoRecipe={ favRecipe } />
+      ))}
     </div>
   );
 }
